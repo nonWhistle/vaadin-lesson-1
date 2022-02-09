f3af2c3e7345faa8005de6624a80e70d89025698
start.vaadin.com <no-reply@vaadin.com>
2022-02-09 23:26:35 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/vaadin_lesson_1/Vaadin_lesson_1View.java
@@ -0,0 +1,33 @@
+package com.example.application.views.vaadin_lesson_1;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Vaadin_lesson_1")
+@Route(value = "Vaadin_lesson_1", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class Vaadin_lesson_1View extends VerticalLayout {
+
+    public Vaadin_lesson_1View() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
